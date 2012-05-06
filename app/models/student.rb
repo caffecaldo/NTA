dd7cfc518fe787ed971ee527c9d7b1dad8d58a4a
Jim Carroll <jim.carroll@gmail.com>
2012-05-06 02:03:44 -0500

    Changed Tests table name to Rank_tests

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,6 +2,7 @@ class Student < ActiveRecord::Base
   belongs_to :rank
   has_many :notes
   has_many :year_end_reviews
+  has_many :tests
   validates_presence_of     :days_carry_forward
   validates_numericality_of :days_carry_forward
   def fullname
--- app/models/test.rb
+++ app/models/test.rb
@@ -1,3 +1,4 @@
 class Test < ActiveRecord::Base
   validates_presence_of  :student_id, :rank_id
+  belongs_to :student
 end
