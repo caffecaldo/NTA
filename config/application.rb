df6ca6e41cc28ba5d5e1f2a538a1d8c22c5f7fa0
Jim Carroll <jim.carroll@gmail.com>
2012-06-05 22:53:04 -0500

    Rolled back application.css change. Added heroku-recommended change to application.rb.

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,8 @@ module NTA
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Adding per Heroku: https://devcenter.heroku.com/articles/rails3x-asset-pipeline-cedar#troubleshooting
+    config.assets.initialize_on_precompile = false
   end
 end
